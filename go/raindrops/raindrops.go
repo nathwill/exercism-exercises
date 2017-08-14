73b2e4152affc1cd9ac4bddecb1b0453d24ccd13
Nathan Williams <nath.e.will@gmail.com>
2017-08-14 10:27:33 -0700

    raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,8 +1,29 @@
+// Package raindrops contains methods related to raindrops
 package raindrops
+
+import "fmt"
 
 const testVersion = 3
 
-func Convert(int) string
+// Convert converts an integer to a raindrop
+func Convert(i int) string {
+	var out string
 
-// Don't forget the test program has a benchmark too.
-// How fast does your Convert convert?
+	// divisible by 3, 5, 7?
+	if i%3 == 0 {
+		out += "Pling"
+	}
+	if i%5 == 0 {
+		out += "Plang"
+	}
+	if i%7 == 0 {
+		out += "Plong"
+	}
+
+	// send result
+	if len(out) == 0 {
+		return fmt.Sprintf("%d", i)
+	}
+
+	return fmt.Sprintf("%s", out)
+}
