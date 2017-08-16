d7fb5c583d6a1ecce7958c2ae516251408d325a2
Nathan Williams <nath.e.will@gmail.com>
2017-08-16 16:58:31 -0700

    error-handling (go)

---
+++ go/error-handling/error_handling.go
@@ -0,0 +1,39 @@
+// Package erratum
+package erratum
+
+const testVersion = 2
+
+func Use(opener ResourceOpener, input string) (err error) {
+	var res Resource
+	// open the resource, retrying in case of TransientError
+	for {
+		res, err = opener()
+		if err != nil {
+			if _, ok := err.(TransientError); ok {
+				continue
+			} else {
+				return err
+			}
+		}
+		break
+	}
+
+	// ensure we close the resource
+	defer res.Close()
+
+	// handle panic with FrobError
+	defer func() {
+		if r := recover(); r != nil {
+			if frobErr, ok := r.(FrobError); ok {
+				res.Defrob(frobErr.defrobTag)
+			}
+			err = r.(error)
+		}
+	}()
+
+	// Frob the resource
+	res.Frob(input)
+
+	// err is either nil or the panic error
+	return err
+}
